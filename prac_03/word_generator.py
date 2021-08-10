47c8989041d378f58db23211ec3f9d0598749512
DanL <79197876+DanL404@users.noreply.github.com>
2021-08-10 15:19:18 +1000

    Add error-checking to validate user input

--- prac_03/word_generator.py
+++ prac_03/word_generator.py
@@ -7,15 +7,27 @@ Another way to get just consonants would be to use string.ascii_lowercase
 """
 import random
 
-VOWELS = "aeiou"
-CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
-word_format = "ccvcvvc"
-word = ""
-for kind in word_format:
-    if kind == "c":
-        word += random.choice(CONSONANTS)
-    else:
-        word += random.choice(VOWELS)
+def main():
+    VOWELS = "aeiou"
+    CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+    print('Enter a string of either "c" or "v" to make a word')
+    word_format = input('Enter string: ').lower()
+    while not is_valid_format(word_format):
+        print('You must only enter "v" or "c" without spaces.')
+        word_format = input('Enter string: ').lower()
+    word = ""
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
+    print(word)
 
-print(word)
+
+def is_valid_format(string):
+    """Determine whether a string is a valid format."""
+    for char in string:
+        if char != 'c' or char != 'v':
+            return False
+    return True
