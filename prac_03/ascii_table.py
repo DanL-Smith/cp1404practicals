e152ddbf46c028331e45b1e50d2f8a97049a7542
DanL <79197876+DanL404@users.noreply.github.com>
2021-08-10 18:21:23 +1000

    Add a function to get a valid number within a specified range

--- prac_03/ascii_table.py
+++ prac_03/ascii_table.py
@@ -9,22 +9,29 @@ MAXIMUM_AMOUNT = 127
 def main():
     character = input('Enter a character: ')
     print(f"The ASCII code for {character} is {ord(character)}")
-    number = int(input(f'Enter a number between {MINIMUM_AMOUNT} and {MAXIMUM_AMOUNT}: '))
-    while not is_valid_number(number):
-        print(f"You must enter a number between {MINIMUM_AMOUNT} and {MAXIMUM_AMOUNT}.")
-        number = int(input(f'Enter a number between {MINIMUM_AMOUNT} and {MAXIMUM_AMOUNT}: '))
+    number = get_valid_number(f'Enter a number between {MINIMUM_AMOUNT} and {MAXIMUM_AMOUNT}: '
+                        , MINIMUM_AMOUNT, MAXIMUM_AMOUNT)
     print(f"The character for {number} is {chr(number)}")
     print()
     display_ascii_values()
 
-def is_valid_number(number):
-    if MINIMUM_AMOUNT <= number <= MAXIMUM_AMOUNT:
-        return True
-    else:
-        return False
+
+def get_valid_number(string, lower, upper):
+    """Get a number within a specified range"""
+    while True:
+        try:
+            number = int(input(string))
+            while number < lower or number > upper:
+                print(f"You must enter a number between {lower} and {upper}.")
+                number = int(input(f'Enter a number between {lower} and {upper}: '))
+            return number
+        except ValueError:
+            print('Please enter a valid number!')
+            continue
 
 
 def display_ascii_values():
+    """Display ASCII value table"""
     print("ASCII values:")
     for i in range(MINIMUM_AMOUNT, MAXIMUM_AMOUNT):
         print(f"{i:<4d} {chr(i)}")
