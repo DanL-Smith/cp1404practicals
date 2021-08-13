29ff61dd78525153ac539733194c6cb9227db907
DanL <79197876+DanL404@users.noreply.github.com>
2021-08-13 16:56:43 +1000

    Initial commit

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,35 @@
+"""
+CP1404/CP5632 - Practical
+A warm-up program to manipulate lists
+"""
+
+
+# What values do the following expressions have?
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers[0] - 3
+# numbers[-1] - 2
+# numbers[3] - 1
+# numbers[:-1] - 314159
+# numbers[3:4] - 1
+# 5 in numbers - True
+# 7 in numbers - False
+# "3" in numbers - False
+# numbers + [6, 5, 3] - 3141592653
+
+def main():
+    numbers = [3, 1, 4, 1, 5, 9, 2]
+
+    # 1. Change the first element of numbers to "ten"
+    # numbers.insert(0, "ten")
+
+    # 2. Change the last element of numbers to 1
+    # numbers.append(1)
+
+    # 3. Get all the elements from numbers except the first two (slice)
+    # print(numbers[2:])
+
+    # 4. Check if 9 is an element
+    # print(9 in numbers)
+
+
+main()
