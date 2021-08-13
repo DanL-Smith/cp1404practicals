48aa985da40c9d7a745814824657044e1fd78261
DanL <79197876+DanL404@users.noreply.github.com>
2021-08-13 22:30:44 +1000

    Initial commit

---
+++ prac_02/word_generator.py
@@ -0,0 +1,32 @@
+"""
+CP1404/CP5632 - Practical
+Random word generator - based on format of words
+
+Another way to get just consonants would be to use string.ascii_lowercase
+(all letters) and remove the vowels.
+"""
+import random
+
+
+def main():
+    VOWELS = "aeiou"
+    CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+    print('Enter a string of either "c" or "v" to make a word')
+    word_format = input('Enter string: ').lower()
+    while not is_valid_format(word_format):
+        print('You must only enter "v" or "c" without spaces.')
+        word_format = input('Enter string: ').lower()
+    word = ""
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
+    print(word)
+
+
+def is_valid_format(string):
+    for char in string:
+        if char != 'c' or char != 'v':
+            return False
+    return True
