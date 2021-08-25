5c9809386b356ce9b3a0852a5fba6923545556be
DanL <79197876+DanL404@users.noreply.github.com>
2021-08-25 15:58:36 +1000

    Add loop to print key-value pairs

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -16,3 +16,5 @@ while state_code != "":
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
+for key, value in CODE_TO_NAME.items():
+    print(f"{key:3s} is {value}")
