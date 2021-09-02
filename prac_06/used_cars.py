c68d92b0e54dc7610c8da0b78dc8d41eb61d3ae0
DanL-Smith <Daniel.Smith10@my.jcu.edu.au>
2021-09-02 12:10:15 +1000

    Add used cars program

---
+++ prac_06/used_cars.py
@@ -0,0 +1,24 @@
+"""CP1404/CP5632 Practical - Client code to use the Car class."""
+from prac_06.car import Car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car(180)
+    my_car.drive(30)
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
+
+    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    limo = Car(100, "Limo")
+    limo.add_fuel(20)
+    print(f"Fuel = {limo.fuel}")
+    limo.drive(115)
+    print(f"Odo = {limo.odometer}")
+    print(limo)
+
+
+main()
