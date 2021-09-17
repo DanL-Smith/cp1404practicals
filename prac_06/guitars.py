ae17f4dd9c9aac433cf9823068994e708e43f17f
DanL-Smith <Daniel.Smith10@my.jcu.edu.au>
2021-09-17 16:28:38 +1000

    Add int function to input calls

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -22,8 +22,8 @@ def get_guitars():
     guitars = []
     name = input("Name: ")
     while name != '':
-        year = input("Year: ")
-        cost = input("Cost: ")
+        year = int(input("Year: "))
+        cost = int(input("Cost: "))
         guitars.append(Guitar(name, year, cost))
         name = input("Name: ")
     return guitars
