f89d24dd02050d8e3797af23b9cbb95219ae5eb2
DanL-Smith <Daniel.Smith10@my.jcu.edu.au>
2021-11-03 20:26:26 +1000

    Fix incomplete compound Boolean expression

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,12 +3,10 @@ email = input("Email: ")
 while email != "":
     modified_email = email.replace(",", " ").replace(".", " ")
     name = " ".join([char for char in (modified_email[:modified_email.find("@")].split()) if not char.isdigit()]).title()
-    confirm = input(f"Is your name {name}? (Y/n)").lower()
-    if confirm == 'y' or "":
-        email_to_name[email] = name
-    elif confirm == 'n' or 'no':
+    confirm = input(f"Is your name {name}? (y/n)").lower()
+    if confirm != 'y' and confirm != '':
         name = input('Name: ')
-        email_to_name[email] = name
+    email_to_name[email] = name
     email = input("Email: ")
 for email, name in email_to_name.items():
     print(f"{name} {email}")
