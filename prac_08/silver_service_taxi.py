cffc5f9941b45d236247542dd522a115b9c77b11
DanL-Smith <Daniel.Smith10@my.jcu.edu.au>
2021-11-05 15:48:40 +1000

    Refactor flagfall variable to no longer represent a constant

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -7,7 +7,7 @@ from prac_08.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Represent a SilverServiceTaxi."""
-    FLAGFALL = 4.50
+    flagfall = 4.50
 
     def __init__(self, name: str = '', fuel: int = 0, fanciness: float = 0):
         """Initialise a SilverServiceTaxi."""
@@ -18,8 +18,8 @@ class SilverServiceTaxi(Taxi):
     def __str__(self):
         """Return a string representation of a SilverServiceTaxi."""
         return f"{super().__str__()}, {self.current_fare_distance}, ${self.PRICE_PER_KM:.2f}/km " \
-               f"plus flagfall of ${self.FLAGFALL} "
+               f"plus flagfall of ${self.flagfall} "
 
     def get_fare(self):
         """Get the current fare."""
-        return self.FLAGFALL + super().get_fare()
+        return self.flagfall + super().get_fare()
